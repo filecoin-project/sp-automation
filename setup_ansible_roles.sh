3d0b7c7dd8e03d632bbf8ed318e3d11fa96d5b07
Benjamin Arntzen <zorlin@gmail.com>
2023-08-10 16:08:03 +0800

    Port ansible role setup scripts

---
+++ setup_ansible_roles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the script is being run from the "sp-stack-automation" folder
+if [ "$(basename "$(pwd)")" != "sp-stack-automation" ]; then
+    echo "Please run this script from the 'sp-stack-automation' folder."
+    exit 1
+fi
+
+# Install Ansible requirements
+ansible-galaxy install -r roles/requirements.yml
+
+# Create symbolic links for each role for development purposes
+ln -sf "$(pwd)/roles/lotus_daemon" "$HOME/.ansible/roles/zorlin.lotus_daemon"
+ln -sf "$(pwd)/roles/lotus_miner" "$HOME/.ansible/roles/zorlin.lotus_miner"
+ln -sf "$(pwd)/roles/fil_boost" "$HOME/.ansible/roles/zorlin.fil_boost"
