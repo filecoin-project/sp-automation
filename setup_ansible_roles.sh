a74991343a1840745f496f11f6a0a1e8382ea166
Benjamin Arntzen <zorlin@gmail.com>
2023-09-13 11:58:02 +0800

    Initial commit of fil_booster_http skeleton

--- setup_ansible_roles.sh
+++ setup_ansible_roles.sh
@@ -13,5 +13,6 @@ ansible-galaxy install -r roles/requirements.yml
 ln -sf "$(pwd)/roles/lotus_daemon" "$HOME/.ansible/roles/zorlin.lotus_daemon"
 ln -sf "$(pwd)/roles/lotus_miner" "$HOME/.ansible/roles/zorlin.lotus_miner"
 ln -sf "$(pwd)/roles/fil_boost" "$HOME/.ansible/roles/zorlin.fil_boost"
+ln -sf "$(pwd)/roles/fil_booster_http" "$HOME/.ansible/roles/zorlin.fil_booster_http"
 ln -sf "$(pwd)/roles/yugabytedb" "$HOME/.ansible/roles/zorlin.yugabytedb"
 ln -sf "$(pwd)/roles/lotus_boost_actors" "$HOME/.ansible/roles/zorlin.lotus_boost_actors"
